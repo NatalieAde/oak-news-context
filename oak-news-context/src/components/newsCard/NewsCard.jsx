9a0888172a83ee1c24023cca19b382e3432717d8
Natalie Adeyinka <nattadey@yahoo.co.uk>
2024-05-02 11:26:10 +0100

    project set up, created components with style files, context and pages with style files

---
+++ oak-news-context/src/components/newsCard/NewsCard.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./NewsCard.module.css";
+
+function NewsCard() {
+  return <div>NewsCard</div>;
+}
+
+export default NewsCard;
---
+++ oak-news-context/src/pages/categories/Categories.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./Categories.module.css";
+
+function Categories() {
+  return <div className={styles.container}>Categories</div>;
+}
+
+export default Categories;
