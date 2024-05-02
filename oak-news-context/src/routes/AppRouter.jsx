5ef48869496cedd21d8b3b7bd108929563d40be7
Natalie Adeyinka <nattadey@yahoo.co.uk>
2024-05-02 12:17:18 +0100

    Created simple route structure

--- oak-news-context/src/routes/AppRouter.jsx
+++ oak-news-context/src/routes/AppRouter.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "../components/header/Header";
+import Home from "../pages/home/Home";
+import Search from "../pages/search/Search";
+import Categories from "../pages/categories/Categories";
 
 function AppRouter() {
-  return <div>AppRouter</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/categories" element={<Categories />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default AppRouter;
