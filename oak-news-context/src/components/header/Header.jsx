7297b5ec5315bea6a088c7b7d2591dbdc4b749ec
Natalie Adeyinka <nattadey@yahoo.co.uk>
2024-05-07 13:42:06 +0100

    added and styled a responsive headder and nav bar

--- oak-news-context/src/components/header/Header.jsx
+++ oak-news-context/src/components/header/Header.jsx
@@ -1,8 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import styles from "./Header.module.css";
+import Navbar from "../navbar/Navbar";
 
-function header() {
-  return <div>header</div>;
+function Header() {
+  const [search, setSearch] = useState("");
+  const navigate = useNavigate();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    navigate("/search", { state: search });
+    setSearch("");
+  };
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <h1>OAK NEWS</h1>
+        <form onSubmit={handleSubmit}>
+          <input
+            className={styles.search}
+            type="text"
+            placeholder="Search for news"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+          />
+          <button type="submit">Search</button>
+        </form>
+      </div>
+      <Navbar />
+    </div>
+  );
 }
 
-export default header;
+export default Header;
--- oak-news-context/src/components/navbar/Navbar.jsx
+++ oak-news-context/src/components/navbar/Navbar.jsx
@@ -1,8 +1,56 @@
 import React from "react";
 import styles from "./Navbar.module.css";
+import { Link, useNavigate } from "react-router-dom";
 
-function navbar() {
-  return <div>navbar</div>;
+function Navbar() {
+  const navigate = useNavigate();
+  return (
+    <ul className={styles.topUl}>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <p>Categories</p>
+        <ul className={styles.bottomUl}>
+          <li
+            onClick={() =>
+              navigate("/categories", { state: { category: "business" } })
+            }
+          >
+            Business
+          </li>
+          <li
+            onClick={() =>
+              navigate("/categories", { state: { category: "general" } })
+            }
+          >
+            General
+          </li>
+          <li
+            onClick={() =>
+              navigate("/categories", { state: { category: "health" } })
+            }
+          >
+            Health
+          </li>
+          <li
+            onClick={() =>
+              navigate("/categories", { state: { category: "science" } })
+            }
+          >
+            Science
+          </li>
+          <li
+            onClick={() =>
+              navigate("/categories", { state: { category: "sports" } })
+            }
+          >
+            Sports
+          </li>
+        </ul>
+      </li>
+    </ul>
+  );
 }
 
-export default navbar;
+export default Navbar;
