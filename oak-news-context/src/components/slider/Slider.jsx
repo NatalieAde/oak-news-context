96e4c9536ad169a230673de98937996f419e3e97
Natalie Adeyinka <nattadey@yahoo.co.uk>
2024-05-21 11:03:11 +0100

    slider on home page displaying first 3 articles

--- oak-news-context/src/components/slider/Slider.jsx
+++ oak-news-context/src/components/slider/Slider.jsx
@@ -1,8 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
+import { FaArrowAltCircleLeft, FaArrowAltCircleRight } from "react-icons/fa";
 import styles from "./Slider.module.css";
 
-function Slider() {
-  return <div>Slider</div>;
+function Slider({ sliderNews }) {
+  const [current, setCurrent] = useState(0);
+  const length = sliderNews.length;
+
+  const nextSlide = () => {
+    setCurrent(current === length - 1 ? 0 : current + 1);
+  };
+
+  const prevSlide = () => {
+    setCurrent(current === 0 ? length - 1 : current - 1);
+  };
+
+  if (!Array.isArray(sliderNews) || length === 0) {
+    return null;
+  }
+
+  return (
+    <div className={styles.slider}>
+      <FaArrowAltCircleLeft className={styles.left} onClick={prevSlide} />
+      <FaArrowAltCircleRight className={styles.right} onClick={nextSlide} />
+      {sliderNews.map((item, index) => {
+        return (
+          <div key={index}>
+            {index === current && (
+              <div className={styles.container}>
+                <img src={item.urlToImage} alt="img" className={styles.image} />
+                <h3>{item.title}</h3>
+                <div className={styles.link}>
+                  <a href={item.url} target="_blank" rel="noreferrer">
+                    Detail
+                  </a>
+                </div>
+              </div>
+            )}
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default Slider;
